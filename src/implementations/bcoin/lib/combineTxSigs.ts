1d72ec7bbee9e91ba2b87b68aa09eacb90789542
Bernd Strehl <bernd.strehl@gmail.com>
2020-08-26 21:18:48 +0200

    add public key to combineTxSigs"

--- src/implementations/bcoin/lib/combineTxSigs.ts
+++ src/implementations/bcoin/lib/combineTxSigs.ts
@@ -1,6 +1,6 @@
 import { PartialTransaction, FullTransaction } from '../../../types';
 
-export async function combineTxSigs(_partialTx: PartialTransaction, _signatures: string[]): Promise<FullTransaction> {
+export async function combineTxSigs(_partialTx: PartialTransaction, _signatures: string[], _signerPublicKey: string): Promise<FullTransaction> {
   // TODO: Implement
   // Takes the signatures of the tosigns created by the 'getTosigns" method and the transaction template
   // to create a complete transaction that is ready to be sent to the network
