5005b5ef416a79e79ba6acfb1ffb9e0d4f566a8d
Bernd Strehl <bernd.strehl@gmail.com>
2020-09-04 11:36:18 +0200

    add public key

--- src/implementations/bcoin/handlers/tx/signAndSendTransaction.ts
+++ src/implementations/bcoin/handlers/tx/signAndSendTransaction.ts
@@ -1,22 +1,22 @@
 import { PartialTransaction, SignAndSendStatus } from '../../../../types';
 import { combineTxSigs } from '../../lib/combineTxSigs';
-import axios from 'axios';
+// import axios from 'axios';
 
 export async function signAndSendTransaction(
   _partialTx: PartialTransaction,
   _signatures: string[],
 ): Promise<SignAndSendStatus> {
   try {
-    const publicKey = '039d200d2ec5c780c4db2ece1f1fecaf63e2e2e68cc967eb06b729f569eddd41c2';
+    const publicKey = '0220e3ae436909dfade128f8f6fcf901a7a20103cb304de59db0646bdd83a00682';
     const signed = await combineTxSigs(_partialTx, _signatures, publicKey);
     if (!signed) {
       return 'INVALID_SIGNATURES';
     }
     console.log(signed);
-    const res = await axios.post(`${process.env.BCOIN_URL}`, { method: 'sendrawtransaction', params: [signed] });
-    if (res.data) {
-      console.log(res.data);
-    }
+    // const res = await axios.post(`${process.env.BCOIN_URL}`, { method: 'sendrawtransaction', params: [signed] });
+    // if (res.data) {
+    //   console.log(res.data);
+    // }
     return 'OK';
   } catch (error) {
     console.log(error);
